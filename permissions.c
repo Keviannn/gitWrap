508b425ce61fb02e578725d73633a459eb28eb00
kevian <joseluisprasie@gmail.com>
2025-11-29 20:16:47 +0100

    Added placeholders

--- permissions.c
+++ permissions.c
@@ -11,3 +11,39 @@ int check_user_permission(const char *user, const char *repository, enum GIT com
     return 1;
 }
 
+int add_user_permission(const char *user, const char *repository, enum GIT command)
+{
+    //TODO: placeholder
+    if(command)
+        printf("Adding permission for %s in %s", user, repository);
+    return 1;
+}
+
+int remove_user_permission(const char *user, const char *repository, enum GIT command)
+{
+    //TODO: placeholder
+    if(command)
+        printf("Removing permission for %s in %s", user, repository);
+    return 1;
+}
+
+int add_all_permissions(const char *user, const char *repository)
+{
+    //TODO: placeholder
+    printf("Adding all permissions for %s in %s", user, repository);
+    return 1;
+}
+
+int remove_all_permissions(const char *user, const char *repository)
+{
+    //TODO: placeholder
+    printf("Removing all permissions for %s in %s", user, repository);
+    return 1;
+}
+
+int list_user_permissions(const char *user, const char *repository)
+{
+    //TODO: placeholder
+    printf("Listing permissions for %s in %s", user, repository);
+    return 1;
+}
