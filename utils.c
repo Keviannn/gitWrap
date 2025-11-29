252aeefcaab9a8996cbdc31e404be81b29b6fbe7
kevian <joseluisprasie@gmail.com>
2025-11-29 22:17:27 +0100

    Removed execute_in_shell as is only used in main.c

--- utils.c
+++ utils.c
@@ -13,17 +13,3 @@ void fperror(const char *msg, ...)
     va_end(args);
     fprintf(stderr, "\n");
 }
-
-int execute_in_shell(char *shell_path, char *command)
-{
-    char *argv[4];
-    argv[0] = shell_path;
-    argv[1] = "-c";
-    argv[2] = command;
-    argv[3] = NULL;
-
-    execv(shell_path, argv);
-    fperror("Could not execute git command: execv failed");
-
-    return 1;
-}
--- utils.h
+++ utils.h
@@ -4,6 +4,3 @@
 
 // Print an error message to stderr with formatted output
 void fperror(const char *msg, ...);
-
-// Execute a command in a specified shell
-int execute_in_shell(char *shell, char *command);
