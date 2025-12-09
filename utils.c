5b0fe58c05954a17dc637b7277d17cb913524a82
kevian <joseluisprasie@gmail.com>
2025-12-09 15:40:43 +0100

    Added check_repository_name function

--- utils.c
+++ utils.c
@@ -3,6 +3,7 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <regex.h>
 
 void fperror(const char *msg, ...)
 {
@@ -12,4 +13,40 @@ void fperror(const char *msg, ...)
     vfprintf(stderr, msg, args);
     va_end(args);
     fprintf(stderr, "\n");
+}
+
+int check_repository_name(const char *name)
+{
+    // Allow NULL names to pass through (handled elsewhere)
+    if (name == NULL)
+        return 1;
+    
+    regex_t regex;
+    int ret;
+
+    const char *pattern = "^\'[A-Za-z0-9_-]+/[A-Za-z0-9_-]+\'$";
+
+    if (regcomp(&regex, pattern, REG_EXTENDED))
+    {
+        fperror("Could not compile regex");
+        return 0;
+    }
+
+    ret = regexec(&regex, name, 0, NULL, 0);
+    regfree(&regex);
+
+    if (!ret)
+        return 1;
+
+    else if(ret == REG_NOMATCH)
+    {
+        fperror("Repository name \"%s\" is not valid", name);
+        return 0;
+    }
+
+    else
+    {
+        fperror("Could not check repository name");
+        return 0;
+    }
 }
--- utils.h
+++ utils.h
@@ -4,3 +4,6 @@
 
 // Print an error message to stderr with formatted output
 void fperror(const char *msg, ...);
+
+// Check if the repository name is valid
+int check_repository_name(const char *name);
