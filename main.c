d1cc04821b2fd9480dbf8445a8095a70c2db8d57
kevian <joseluisprasie@gmail.com>
2025-12-11 14:06:13 +0100

    Multiple changes - Only apply fixing path if the command is not GIT_CREATE - Only get SSH_USER environment variable once - Handling of permissions on repository creation - Check repository name lenght - Error messages now use MSG_TYPE - Handled options for debug - Added GIT_DELETE case for future implementation

--- main.c
+++ main.c
@@ -24,7 +24,7 @@ int execute_in_shell(char *shell_path, char *command)
     argv[3] = NULL;
 
     execv(shell_path, argv);
-    fperror("Could not execute git command: execv failed");
+    fperror(MSG_ERROR, "Could not execute git command: execv failed");
 
     return 1;
 }
@@ -41,7 +41,7 @@ char *fix_path(const char *path)
 
         if (asprintf(&fixed, "%s/%s", "\'~", path) == -1)
         {
-            fperror("Could not allocate memory for fixed");
+            fperror(MSG_ERROR, "Could not allocate memory for fixed");
             return NULL;
         }
     }
@@ -56,13 +56,13 @@ int change_to_gitwrap_dir()
 
     if (asprintf(&path, "%s/%s", getenv("HOME"), LOCATION) == -1) 
     {
-        fperror("Memory allocation error");
+        fperror(MSG_ERROR, "Memory allocation error");
         return 1;
     }
 
     if (chdir(path) != 0)
     {
-        fperror("Could not change directory to %s", path);
+        fperror(MSG_ERROR, "Could not change directory to %s", path);
         return 1;
     }
 
@@ -95,43 +95,79 @@ int main()
 
     // If repository name is not valid stop execution
     if (!check_repository_name(repository))
-        return 1;
-
-    // Fix the repository path
-    char *real_path = fix_path(repository);
-    char *final_command = NULL;
-
-    // Check for memory allocation error
-    if (asprintf(&final_command, "%s %s", command, real_path) == -1)
     {
-        fperror("Could not allocate memory for final_command");
-        return 1;
+        // Debug lines
+        if(DEBUG)
+        {
+            if (classify_command(command) == GIT_CREATE)
+                fperror(MSG_DEBUG, "Repository name is valid for the command %s\n", command);
+            else
+                return 1;
+        }
+
+        else
+        {
+            if (!(classify_command(command) == GIT_CREATE))
+                return 1;
+        }
     }
+
+    char *final_command = NULL;
+    char *real_path = NULL;
+
+    // Fix the repository path if command is not create
+    if(classify_command(command) != GIT_CREATE)
+    {
+        real_path = fix_path(repository);
+        
+        // Create the final command to execute
+        if (asprintf(&final_command, "%s %s", command, real_path) == -1)
+        {
+            fperror(MSG_ERROR, "Could not allocate memory for final_command");
+            return 1;
+        }
+    }
+
+    char *user = getenv("SSH_USER");
 
     // Classify and execute the command
     switch (classify_command(command)) {
         case GIT_WELCOME:
-            printf("Hi %s! You've successfully authenticated!\n", getenv("SSH_USER"));
+            printf("Hi %s! You've successfully authenticated!\n", user);
             printf("But you can't log in with a shell :(\n\n");
             printf("Bye!\n");
             break;
         case GIT_CREATE:
-            setenv("REPO_NAME", repository, 1);
-
-            add_all_permissions(getenv("SSH_USER"), repository);
-
-            //TODO: check repository lenght before accepting max line is 256 for conf file
-
-            if (execute_in_shell(SHELL, CREATE_REPOSITORY))
+            fperror(MSG_DEBUG, "Trying to create repository for user %s\n", user);
+            
+            if(!add_own_permission(user, repository))
             {
-                remove_all_permissions(getenv("SSH_USER"), real_path);
-                return 1;
+                fperror(MSG_ERROR, "No permissions were added\n");
+                break;
             }
+
+            // Check if repository name is too long
+            if (strlen(repository) > 256)
+            {
+                fperror(MSG_ERROR, "Repository name too long\n");
+                break;
+            }
+
+            //TODO: review if create repository could fail in order to rollback permission addition
+
+            //TODO: change script form bash to C
+            setenv("REPO_NAME", repository, 1);
+            if (execute_in_shell(SHELL, CREATE_REPOSITORY))
+                return 1;
+
+            break;
+        case GIT_DELETE:
+            //TODO: git DELETE implementation
             break;
         case GIT_PUSH:
-            if (check_user_permission(getenv("SSH_USER"), real_path, GIT_PUSH) != 1) 
+            if (check_user_permission(user, real_path, GIT_PUSH) != 1) 
             {
-                fperror("Permission denied for command: %s\n", command);
+                fperror(MSG_ERROR, "Permission denied for command: %s\n", command);
                 return 1;
             }
             if (execute_in_shell(GIT_SHELL, final_command))
@@ -139,16 +175,16 @@ int main()
             break;
         case GIT_PULL:
             // Check if the user has pull permission (for private repositories)
-            if (check_user_permission(getenv("SSH_USER"), real_path, GIT_PULL) != 1) 
+            if (check_user_permission(user, real_path, GIT_PULL) != 1) 
             {
-                fperror("Permission denied for command: %s\n", command);
+                fperror(MSG_ERROR, "Permission denied for command: %s\n", command);
                 return 1;
             }
             if (execute_in_shell(GIT_SHELL, final_command))
                 return 1;
             break;
         case GIT_NOT_ALLOWED:
-            fperror("Command not allowed: %s\n", command);
+            fperror(MSG_ERROR, "Command not allowed: %s\n", command);
             break;
     }
 
