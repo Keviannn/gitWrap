af90714290f0be65c3aed6bc9f7f9872e356e273
kevian <joseluisprasie@gmail.com>
2025-12-11 14:30:20 +0100

    Multiple changes - Implemented add_own_permission for repository creation - Added placehorlder for remove_entries for repository deletion - Removed functions that are now not planned - Error messages now use MSG_TYPE

--- permissions.c
+++ permissions.c
@@ -9,6 +9,7 @@
 
 #define USERS_FILE "../test.conf"
 
+// Extracts the repository name from the full repository path
 char* get_repo_name(char *repository_path)
 {
     // Copies original path
@@ -17,7 +18,7 @@ char* get_repo_name(char *repository_path)
     // Checks if the copy was successful
     if (repository_cp == NULL)
     {
-        fperror("Could not allocate memory for repository_format");
+        fperror(MSG_ERROR, "Could not allocate memory for repository_format");
         free(repository_cp);
         return NULL;
     }
@@ -32,7 +33,7 @@ char* get_repo_name(char *repository_path)
     // Checks if the copy was successful
     if (repository_name == NULL)
     {
-        fperror("Could not allocate memory for repository_name");
+        fperror(MSG_ERROR, "Could not allocate memory for repository_name");
         free(repository_name);
         return NULL;
     }
@@ -44,13 +45,14 @@ char* get_repo_name(char *repository_path)
     return repository_name;
 }
 
+// Check if a user has permission to execute a specific Git command on a repository
 int check_user_permission(const char *user, const char *repository_path, enum GIT command) 
 {
     FILE *file = fopen(USERS_FILE, "r");
 
     if (file == NULL)
     {
-        fperror("Could not opnen users file");
+        fperror(MSG_ERROR, "Could not opnen users file");
         return 0;
     }
 
@@ -60,7 +62,7 @@ int check_user_permission(const char *user, const char *repository_path, enum GI
 
     if(asprintf(&user_format, "[%s]", user) == -1)
     {
-        fperror("Could not allocate memory for user_format");
+        fperror(MSG_ERROR, "Could not allocate memory for user_format");
         return 0;
     }
 
@@ -78,17 +80,15 @@ int check_user_permission(const char *user, const char *repository_path, enum GI
         if (line[0] == '#' || line[0] == '\n')
             continue;
 
-        fperror("Checking line: %s", line); // Debug line
-
         // Get only the line without the newline
         strtok(line, "\n"); 
     
         // If the line is the line with the user we are looking
         if (strcmp(line, user_format) == 0)
         {
-            fperror("Found user %s\n", user); // Debug line
+            fperror(MSG_DEBUG, "Found user %s\n", user);
 
-            fperror("Checking permissions for repository %s\n", repository_name); // Debug line
+            fperror(MSG_DEBUG, "Checking permissions for repository %s\n", repository_name);
             // Get more lines until we find an empty line
             while(fgets(line, sizeof(line), file)[0] != '\n')
             {
@@ -100,7 +100,7 @@ int check_user_permission(const char *user, const char *repository_path, enum GI
                 // If the line contains the repository name and the word own in it
                 if (strstr(line, repository_name) != NULL && strstr(line, OWNER) != NULL)
                 {
-                    fperror("User %s is owner of %s\n", user, repository_name); // Debug line
+                    fperror(MSG_DEBUG, "User %s is owner of %s\n", user, repository_name);
                     // User has all permissions so anything is granted
                     grant = 1;
                     break;
@@ -111,7 +111,7 @@ int check_user_permission(const char *user, const char *repository_path, enum GI
                 {
                     if (command == GIT_PULL)
                     {
-                        fperror("User %s has read permission for %s\n", user, repository_name); // Debug line
+                        fperror(MSG_DEBUG, "User %s has read permission for %s\n", user, repository_name);
                         grant = 1;
                         break;
                     }
@@ -122,7 +122,7 @@ int check_user_permission(const char *user, const char *repository_path, enum GI
                 {
                     if (command == GIT_PUSH)
                     {
-                        fperror("User %s has write permission for %s\n", user, repository_name); // Debug line
+                        fperror(MSG_DEBUG, "User %s has write permission for %s\n", user, repository_name);
                         grant = 1;
                         break;
                     }
@@ -131,7 +131,7 @@ int check_user_permission(const char *user, const char *repository_path, enum GI
 
             // If we find an empty line before finding the repository we dont have permissions
             if (grant == 0)
-                fperror("User %s has no permissions for %s\n", user, repository_name);
+                fperror(MSG_ERROR, "User %s has no permissions for %s\n", user, repository_name);
 
             // If we find the user but no permissions stop looking
             break;
@@ -148,33 +148,96 @@ int check_user_permission(const char *user, const char *repository_path, enum GI
     return grant;
 }
 
-int add_user_permission(const char *user, const char *repository, enum GIT command)
+int add_own_permission(const char *user, const char *repository_name)
 {
-    //TODO: placeholder
-    if(command)
-        printf("Adding permission for %s in %s", user, repository);
-    return 1;
+    FILE *file = fopen(USERS_FILE, "r+");
+    FILE *temp_file = fopen("temp.conf", "w");
+
+    if (file == NULL || temp_file == NULL)
+    {
+        fperror(MSG_ERROR, "Could not open users file");
+        return 0;
+    }
+
+    char line[256];
+
+    char *user_format = NULL;
+
+    if(asprintf(&user_format, "[%s]", user) == -1)
+    {
+        fperror(MSG_ERROR, "Could not allocate memory for user_format");
+        return 0;
+    }
+
+    int added = 0;
+    int found = 0;
+
+    // Check every line in the file
+    while (fgets(line, sizeof(line), file))
+    {
+        // Write the line to the temporary file
+        fwrite(line, sizeof(char), strlen(line), temp_file);
+
+        // Get only the line without the newline
+        strtok(line, "\n"); 
+    
+        // If the line is the line with the user we are looking
+        if (strcmp(line, user_format) == 0)
+        {
+            fperror(MSG_DEBUG, "Found user %s\n", user);
+            // Get more lines until we find an empty line
+            while(fgets(line, sizeof(line), file)[0] != '\n')
+            {
+                // Write the line to the temporary file
+                fwrite(line, sizeof(char), strlen(line), temp_file);
+
+                // Ignore comments
+                if (line[0] == '#')
+                    continue;
+
+                // If the line contains the repository name
+                if (strstr(line, repository_name))
+                {
+                    fperror(MSG_ERROR, "There are already permissions for %s under the user %s\n", repository_name, user);
+                    found = 1;
+                    break;
+                }
+            }
+
+            if (!found)
+            {
+                // If we find an empty line we add permissions
+                fperror(MSG_DEBUG, "Adding permissions for %s under the user %s\n", repository_name, user);
+                char *perm_line = NULL;
+                asprintf(&perm_line, "%s = %s\n\n", repository_name, "own");
+                fperror(MSG_DEBUG, "Permission line to add: %s\n", perm_line);
+                fwrite(perm_line, sizeof(char), strlen(perm_line), temp_file);
+                free(perm_line);
+                added = 1;
+            }
+        }
+    }
+
+    // Free allocated strings
+    free(user_format);
+
+    // Close files
+    fclose(file);
+    fclose(temp_file);
+
+    // Replace original file with temporary file
+    remove(USERS_FILE);
+    rename("temp.conf", USERS_FILE);
+
+    return added;
 }
 
-int remove_user_permission(const char *user, const char *repository, enum GIT command)
+// TODO: implement for fail to create repositories cases
+int remove_entries(const char *repository_name)
 {
-    //TODO: placeholder
-    if(command)
-        printf("Removing permission for %s in %s", user, repository);
-    return 1;
-}
-
-int add_all_permissions(const char *user, const char *repository)
-{
-    //TODO: placeholder
-    printf("Adding all permissions for %s in %s", user, repository);
-    return 1;
-}
-
-int remove_all_permissions(const char *user, const char *repository)
-{
-    //TODO: placeholder
-    printf("Removing all permissions for %s in %s", user, repository);
+    // Placeholder
+    if (repository_name == NULL)
+        return 1;
     return 1;
 }
 
--- permissions.h
+++ permissions.h
@@ -12,16 +12,7 @@ static const char WRITE[] = "write";
 int check_user_permission(const char *user, const char *repository_path, enum GIT command);
 
 // Add a specific Git command permission for a user on a repository
-int add_user_permission(const char *user, const char *repository, enum GIT command);
-
-// Remove a specific Git command permission for a user on a repository
-int remove_user_permission(const char *user, const char *repository, enum GIT command);
-
-// Add all Git command permissions for a user on a repository
-int add_all_permissions(const char *user, const char *repository);
-
-// Remove all Git command permissions for a user on a repository
-int remove_all_permissions(const char *user, const char *repository);
+int add_own_permission(const char *user, const char *repository_path);
 
 // List all permissions for a user on a repository
 int list_user_permissions(const char *user, const char *repository);
